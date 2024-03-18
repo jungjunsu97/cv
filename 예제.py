b3ea97fcd2cb3e99a5d5eac3baa4847446225898
jungjunsu97 <163382162+jungjunsu97@users.noreply.github.com>
2024-03-18 09:11:20 +0900

    Add files via upload

---
+++ 예제.py
@@ -0,0 +1,14 @@
+# 3-1
+print(Hello world!)
+print(Hello  world!)
+print("Hello" + "world")
+print("Hello" + "" + "world")
+
+# 3-2
+print("오늘은 파이썬 챌린지 3일차 입니다")
+print('디버깅 연습중입니다')
+print('내일은("금요일" + "입니다)')
+print("그래서 챌린지가 없습니다")
+
+# 3-3
+print("나는input("당신의 이름은 무엇입니까?"))
